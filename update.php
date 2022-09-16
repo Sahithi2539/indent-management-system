ad8aaf91642fd05f738af760209203334a30c910
Vinnakota Lakshmi sahithi <69851403+Sahithi2539@users.noreply.github.com>
2022-09-16 22:17:50 +0530

    Add files via upload

--- update.php
+++ update.php
@@ -1,9 +1,14 @@
-<?php 
-include('database.php');
-include("connection.php");
-$uid = "$_GET[title]";
-// print($uid);
-$APP='APPROVED';
-$sql = "UPDATE owner SET Approval='APPROVED' WHERE uniqueID=$uid";
-header("location:description-indent.php?title=$uid");  
+<?php
+$link = mysqli_connect("localhost", "root", "", "javatpoint");
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+$sql = "UPDATE owner SET Approval='Approved' WHERE uniqueID='$_GET[title]'";
+if(mysqli_query($link, $sql)){
+    echo "Records were updated successfully.";
+} else {
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+mysqli_close($link);
+header("location:description-indent.php?title=$_GET[title]");  
 ?>
